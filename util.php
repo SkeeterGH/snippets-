9ca07e58d8822f0c8ee8ce99a75d588978dec9ee
Abukari Sumaila <91499352+SkeeterGH@users.noreply.github.com>
2023-01-13 21:20:50 +0000

    Update util.php

--- util.php
+++ util.php
@@ -1,1 +1,20 @@
+<?php
 
+    class Util{
+
+     //settings
+      static $SecretToken ="";
+      
+      static $ClientToken ="";
+      
+      static  $ServiceID = "ffe133db5500d80342e96bd0b1d813a251b4ed90fe3055c1138595a58e5331f7";
+
+      static $Service = "sandbox"; //sandbox
+
+      static $CounrtyCode = "+233";
+
+      static $SenderName = "Smart Cast";
+
+
+    }
+?>
